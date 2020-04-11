7f0d5d8cfcef6b8f54006187f817625c34bc77d9
LionHearted56 <enecastro@knights.ucf.edu>
2020-04-11 16:31:39 -0400

    Executed script on LAMP server.

--- database/baseMarrketdb.sql
+++ database/baseMarrketdb.sql
@@ -4,7 +4,7 @@ create database basedDealsdb;
 
 use basedDealsdb;
 
-create table account (
+create table user_account (
     username varchar(30) not null,
     uid int(8) not null auto_increment,
     name varchar(30),
@@ -15,7 +15,7 @@ create table account (
     dateCreated timestamp,
     shopkeeper boolean,
     primary key (uid)
-)
+);
 
 create table product (
     productname varchar(30),
@@ -27,23 +27,21 @@ create table product (
     primary key (pid)
 );
 
-create table group (
-    orderId int(8) not null,
-    uid int(8) not null,
-    primary key (orderId, uid),
-    foreign key (orderId)
-    references order(orderId),
-    foreign key (uid)
-    references account(uid)
-);
-
-create table order (
+create table product_order (
     discount float(2,1) not null,
     orderId int(8) not null auto_increment,
     uid int(8) not null,
     pid int(8) not null,
     dateCreated timestamp,
     primary key (orderId),
-    foreign key (uid),
-    foreign key(pid),
+    foreign key (uid) references user_account(uid),
+    foreign key(pid) references product(pid)
+);
+
+create table discount_group (
+    orderId int(8) not null,
+    uid int(8) not null,
+    primary key (orderId, uid),
+    foreign key (orderId) references product_order(orderId),
+    foreign key (uid) references user_account(uid)
 );
