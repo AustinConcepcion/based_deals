b87b8c452b8e63de41582cceea318568d6918f86
LionHearted56 <enecastro@knights.ucf.edu>
2020-04-23 17:04:00 -0400

    Create new user

--- api/CreateUser.php
+++ api/CreateUser.php
@@ -5,14 +5,13 @@ $error = 'none';
 
 try {
     // get json data
-    $data = json_decode(file_get_contents('php://input'));
-    $username = $data->username;
-    $email = $data->email;
-    $password = $data->password;
-    $shopkeeper = $data->shopkeeper;
+    $username = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $shopkeeper = $_POST['shopkeeper'];
 
     //verify data
-    if (empty($username) || empty($name) || empty($email) || empty($password) || empty($address) || empty($creditinfo) || empty($shopkeeper)) {
+    if (empty($username) || empty($email) || empty($password) || empty($shopkeeper)) {
         throw new Exception('Empty field.');
     }
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
@@ -21,21 +20,15 @@ try {
     if (!preg_match('/^[a-zA-Z0-9]*$/', $username)) {
         throw new Exception('Invalid username.');
     }
-    if (!preg_match('/^[a-zA-Z]*$/', $name)) {
-        throw new Exception('Invalid name');
-    }
-    if (!preg_match('/^[0-9]*$/', $name) || strlen($name) < 15 || strlen($name) > 19) {
-        throw new Exception('Invalid credit card info');
-    }
     if (!filter_var($shopkeeper, FILTER_VALIDATE_BOOLEAN)) {
         throw new Exception('Invalid shop keeper');
     }
 
-    // create sql querie
-    $sql = 'INSERT user_account (username, name, email, password, address, creditinfo, shopkeeper, dateCreated) VALUES(?, ?, ?, ?, ?, ?, ?, NOW())';
+    // create sql query
+    $sql = 'INSERT user_account (username, email, password, shopkeeper, dateCreated) VALUES(?, ?, ?, ?, NOW())';
     $stmt = mysqli_stmt_init($conn);
     if (mysqli_stmt_prepare($stmt, $sql)) {
-        mysqli_stmt_bind_param($stmt, 'ssssssi', $username, $name, $email, $password, $address, $creditinfo, ($shopkeeper ? 1 : 0));
+        mysqli_stmt_bind_param($stmt, 'sssi', $username, $email, $password, $creditinfo, ($shopkeeper ? 1 : 0));
         mysqli_stmt_execute($stmt);
     } else {
         throw new Exception('MySQL error.');
@@ -44,4 +37,5 @@ try {
     $error = $e->getMessage();
 } finally {
     echo '{ "error": "'.$error.'" }';
+    header('Location: ../login.html?error='.$error);
 }
