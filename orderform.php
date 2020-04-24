c22f590e7da0a265765ca72dca1b28d3ba0fbcf0
LionHearted56 <enecastro@knights.ucf.edu>
2020-04-24 14:57:49 -0400

    Added ccinfo

--- orderform.php
+++ orderform.php
@@ -24,24 +24,71 @@
     <h2> Item Details: </h2>
     <div style="border: 1px solid; padding:1rem">
       <h3> Item Details </h3>
-      <p> Order ID: this.orderId </p>
+      <p> Order ID: <?php echo $_GET['orderid']; $orderid = $_GET['orderid']; ?> </p>
 
-      <p> Product: SELECT p.productname
-                   FROM product p, product_order o
-                   WHERE o.pid = p.pid
-                   AND o.orderId = this.orderId  </p>
+      <p> Product: 
+        <?php
+          include './includes/db.inc.php';
+          $stmt = mysqli_stmt_init($conn);
 
-      <p> Base Price: SELECT p.price
-                      FROM product p, product_order o
-                      WHERE o.pid = p.pid
-                      AND o.orderId = this.orderId  </p>
+          $sql = 'SELECT p.productname
+          FROM product p, product_order o
+          WHERE o.pid = p.pid
+          AND o.orderId = ?';
+            if (mysqli_stmt_prepare($stmt, $sql)) {
+                mysqli_stmt_execute($stmt);
+                mysqli_stmt_bind_param($stmt, 'i', $orderid);
+                $result = mysqli_stmt_get_result($stmt);
+                while ($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
+                    echo $row[0];
+                }
+            }
+        ?></p>
 
-      <p> Number of items: SELECT COUNT(*)
-                           FROM discount_group d, product_order o
-                           WHERE d.orderId = this.orderId </p>
-      <p> Discount Percent: SELECT o.discount
-                            FROM product_order o
-                            where o.orderId = this.orderId</p>
+      <p> Base Price:
+        <?php
+          $sql = 'SELECT p.price
+          FROM product p, product_order o
+          WHERE o.pid = p.pid
+          AND o.orderId = ?';
+            if (mysqli_stmt_prepare($stmt, $sql)) {
+                mysqli_stmt_execute($stmt);
+                mysqli_stmt_bind_param($stmt, 'i', $orderid);
+                $result = mysqli_stmt_get_result($stmt);
+                while ($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
+                    echo $row[0];
+                }
+            }
+        ?>   </p>
+
+      <p> Number of items: 
+        <?php
+          $sql = 'SELECT COUNT(*)
+          FROM discount_group d, product_order o
+          WHERE d.orderId = ?';
+            if (mysqli_stmt_prepare($stmt, $sql)) {
+                mysqli_stmt_execute($stmt);
+                mysqli_stmt_bind_param($stmt, 'i', $orderid);
+                $result = mysqli_stmt_get_result($stmt);
+                while ($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
+                    echo $row[0];
+                }
+            }
+        ?> </p>
+      <p> Discount Percent: 
+        <?php
+          $sql = 'SELECT o.discount
+          FROM product_order o
+          where o.orderId = ?';
+            if (mysqli_stmt_prepare($stmt, $sql)) {
+                mysqli_stmt_execute($stmt);
+                mysqli_stmt_bind_param($stmt, 'i', $orderid);
+                $result = mysqli_stmt_get_result($stmt);
+                while ($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
+                    echo $row[0];
+                }
+            }
+        ?></p>
 
       <p> Total Price: javascript math stuff needed here
                       (base_price * number_items) - (base_price * number_items * discount) </p>
@@ -51,20 +98,54 @@
     <div style="border: 1px solid; padding:1rem">
 
       <h3> Primary Customer </h3>
-      <p>Name: SELECT u.name
-               FROM user_account u, product_order o
-               WHERE o.oid = this.orderId
-               AND u.uid = o.uid </p>
+      <p>Name: 
+      <?php
+          $sql = 'SELECT u.name
+          FROM user_account u, product_order o
+          WHERE o.oid = ?
+          AND u.uid = o.uid ';
+            if (mysqli_stmt_prepare($stmt, $sql)) {
+                mysqli_stmt_execute($stmt);
+                mysqli_stmt_bind_param($stmt, 'i', $orderid);
+                $result = mysqli_stmt_get_result($stmt);
+                while ($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
+                    echo $row[0];
+                }
+            }
+        ?></p>
 
-      <p>Address: SELECT u.address
-                  FROM user_account u, product_order o
-                  WHERE o.oid = this.orderId
-                  AND u.uid = o.uid </p>
+      <p>Address: 
+      <?php
+          $sql = 'SELECT u.address
+          FROM user_account u, product_order o
+          WHERE o.oid = ?
+          AND u.uid = o.uid';
+            if (mysqli_stmt_prepare($stmt, $sql)) {
+                mysqli_stmt_execute($stmt);
+                mysqli_stmt_bind_param($stmt, 'i', $orderid);
+                $result = mysqli_stmt_get_result($stmt);
+                while ($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
+                    echo $row[0];
+                }
+            }
+        ?>
+         </p>
 
-      <p>Payment Info: SELECT u.creditInfo
-                       FROM user_account u, product_order o
-                       WHERE o.oid = this.orderId
-                       AND u.uid = o.uid </p>
+      <p>Payment Info: 
+      <?php
+          $sql = 'SELECT u.creditInfo
+          FROM user_account u, product_order o
+          WHERE o.oid = ?
+          AND u.uid = o.uid';
+            if (mysqli_stmt_prepare($stmt, $sql)) {
+                mysqli_stmt_execute($stmt);
+                mysqli_stmt_bind_param($stmt, 'i', $orderid);
+                $result = mysqli_stmt_get_result($stmt);
+                while ($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
+                    echo $row[0];
+                }
+            }
+        ?> </p>
 
     </div>
     <h3> Thank you for shopping at Lil B's one stop shop for your candle needs! </h3>
